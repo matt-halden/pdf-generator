d216baca814aa4e298b368b5d2b11ae96be8131e
matt <mjahalden@gmail.com>
2024-04-16 10:08:46 -0700

    Create minimal PDF from excel invoices

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fpdf import FPDF
 import pandas as pd
-# Create standard list for excel files
+# Create standard list for excel files with glob
 import glob
+from fpdf import FPDF
+# pathlib allows us to extract file name
+from pathlib import Path
 
 # grab everything with .xlsx
 filepaths = glob.glob("invoices/*.xlsx")
@@ -12,3 +14,12 @@ for filepath in filepaths:
     # have to install py package: openpyxl for pandas to read excel file
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     print(df)
+    pdf = FPDF(orientation="P", unit='mm', format="A4")
+    pdf.add_page()
+    # get filenames, Path gets us a special string, then using stem grabs just the name
+    filename = Path(filepath).stem
+    # just grab initial invoice number, split makes the full filename into a list
+    invoice_nr = filename.split("-")[0]
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Invoice #: {invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
