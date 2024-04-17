d329ce7509b6807e7877ea089429cce8a456408f
matt <mjahalden@gmail.com>
2024-04-17 09:47:33 -0700

    added invoice date

--- main.py
+++ main.py
@@ -13,13 +13,23 @@ for filepath in filepaths:
     # read_excel not csv as this is an excel file, have to provide sheet_name
     # have to install py package: openpyxl for pandas to read excel file
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+
     pdf = FPDF(orientation="P", unit='mm', format="A4")
     pdf.add_page()
+
     # get filenames, Path gets us a special string, then using stem grabs just the name
     filename = Path(filepath).stem
     # just grab initial invoice number, split makes the full filename into a list
     invoice_nr = filename.split("-")[0]
+    # grab date, extract second item from list. could do invoice_nr, date = filename.split("-")!!!
+    date = filename.split("-")[1]
+
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Invoice #: {invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice #: {invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
+
+
     pdf.output(f"PDFs/{filename}.pdf")
